c5263adb8e8395e5766cb86fafe45b64ef392d41
saket-maz <saket@mazsystems.com>
2021-06-16 20:29:43 +0530

    func rect

---
+++ src/Component/Card.js
@@ -0,0 +1,8 @@
+const Card = (props) => {
+  return<div>
+    <h3>{props.title}</h3>
+    <p><i>Lorem ipsum dolor sit amet.</i></p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero blanditiis ullam animi possimus quidem? Dolor natus sint necessitatibus eveniet ab nostrum dolorem harum officiis quibusdam doloremque doloribus impedit sapiente culpa deserunt, vero atque repellendus aspernatur aperiam voluptatum magni molestias incidunt voluptatem? Ullam soluta voluptatem molestiae magnam. Deleniti ad ducimus quaerat eius aut cum impedit necessitatibus quos rerum et cupiditate laudantium officiis dicta odit maxime unde recusandae, fuga adipisci dolore soluta, praesentium totam, nemo dolores. Corrupti, deserunt provident, voluptatibus sapiente esse ut rem porro voluptas, facilis fugiat hic quis fuga eos? Pariatur dolore architecto distinctio dolor perferendis exercitationem consequuntur ab dicta.</p>
+  </div>
+}
+export default Card;
